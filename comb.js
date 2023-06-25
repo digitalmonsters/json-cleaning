ef8ed86bcb3b803e279e4e362b721f57be763ffc
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-06-25 18:55:38 +0530

    first commit

--- comb.js
+++ comb.js
@@ -1,10 +1,11 @@
-const fs = require('fs'); 
+const fs = require('fs');
+let allOBJ = []
 
 let normalizedPath = require("path").join(__dirname);
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-    let ext = file.substring(file.lastIndexOf('.')+1, file.length) || file;
-	let verb = file.substring(0, file.lastIndexOf('.'));
-    if (ext=="json" && verb.indexOf("package")<0) {
+require("fs").readdirSync(normalizedPath).forEach(function (file) {
+    let ext = file.substring(file.lastIndexOf('.') + 1, file.length) || file;
+    let verb = file.substring(0, file.lastIndexOf('.'));
+    if (ext == "json" && verb.indexOf("package") < 0) {
         console.log("file --> ", file);
 
         cleanFile(require(require("path").join(__dirname, file)), verb)
@@ -13,21 +14,21 @@ require("fs").readdirSync(normalizedPath).forEach(function(file) {
 
 
 function cleanFile(obj, fnm) {
-    let trash = ["PrivateHttpPort","SoundStripe","S3","QrConfig","KafkaWriterConfig","OnlineTimeoutMs","KafkaWriter","MigrationEnv",
-    "HttpPort","HttpDeepLinkPort","MaxThresholdHours","Firebase","WorkerPoolSize","MaxStmtsPerBatch","ContentWrapper",
-    "MasterDb","CategoryTopUserGenerator",
-    "ReadonlyDb","SolanaConfig","EvmConfig","TapjoyConfig","Applovin","Ironsource","Kyc","GoHighLevel","ExportRecordsPerIteration","DeepLinkConfig",
-    "Scylla","ZammadConfig",
-    "Db","CdcKafka",
-    "Redis","Jobber","MobileConfig","PermissionsTable","AppleConfig","GeolocationApiUrl","PreviewConfig","DeeplinkConfig",
-    "Elastic",
-    "Wrappers",
-    "GoogleConfig", "CdnBase"]
-    let trashkeys = ["url", "scylla", "db", "s3", "feedgenerator", "redis"] 
+    let trash = ["PrivateHttpPort", "SoundStripe", "S3", "QrConfig", "KafkaWriterConfig", "OnlineTimeoutMs", "KafkaWriter", "MigrationEnv",
+        "HttpPort", "HttpDeepLinkPort", "MaxThresholdHours", "Firebase", "WorkerPoolSize", "MaxStmtsPerBatch", "ContentWrapper",
+        "MasterDb", "CategoryTopUserGenerator",
+        "ReadonlyDb", "SolanaConfig", "EvmConfig", "TapjoyConfig", "Applovin", "Ironsource", "Kyc", "GoHighLevel", "ExportRecordsPerIteration", "DeepLinkConfig",
+        "Scylla", "ZammadConfig",
+        "Db", "CdcKafka", "MinContentDuration", "MaxPoolSize",
+        "Redis", "Jobber", "MobileConfig", "PermissionsTable", "AppleConfig", "GeolocationApiUrl", "PreviewConfig", "DeeplinkConfig",
+        "Elastic",
+        "Wrappers",
+        "GoogleConfig", "CdnBase"]
+    let trashkeys = ["url", "scylla", "db", "s3", "feedgenerator", "redis"]
 
 
     trashfromconfig = ["WorkerPoolSize", "PollTimeMs", "MaxDuration", "MaxBatchSize", "Tls", "FlushAtSize", "Hosts", "MinBytes", "MaxBytes", "NumPartitions",
-    "ReplicationFactor", "MaxRetryCount", "FlushTimeMilliseconds"]
+        "ReplicationFactor", "MaxRetryCount", "FlushTimeMilliseconds"]
 
     trash.map(t => {
         delete obj[t]
@@ -38,7 +39,7 @@ function cleanFile(obj, fnm) {
 
         Object.keys(obj).map(t => {
             trashkeys.map(tk => {
-            if (t.toLocaleLowerCase().indexOf(tk) >= 0) {
+                if (t.toLocaleLowerCase().indexOf(tk) >= 0) {
                     delete obj[t]
                 }
             })
@@ -50,32 +51,33 @@ function cleanFile(obj, fnm) {
 
     }
     funcClean(obj)
-    
+
     Object.keys(obj).map(e => {
-        trashfromconfig.map(p=>{
+        trashfromconfig.map(p => {
             delete obj[e][p]
 
         })
     })
 
-    Object.keys(obj).map(e => {
-        let dir = require("path").join(__dirname, "cleaned", fnm)
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
+    allOBJ.push({["repo-name"] : fnm, ...obj});
+    // Object.keys(obj).map(e => {
+        // let dir = require("path").join(__dirname, "cleaned", fnm)
+        // if (!fs.existsSync(dir)) {
+        //     fs.mkdirSync(dir);
+        // }
 
-        let newfile = require("path").join(dir, `${e}.json`)
-        fs.writeFileSync(newfile, JSON.stringify(obj[e], null, 4));
+        // let newfile = require("path").join(dir, `${e}.json`)
+        // fs.writeFileSync(newfile, JSON.stringify(obj[e], null, 4));
         // console.log("e------", newfile);
-    })
+    // })
 
 
-    
-    
+
+
     let newfile = require("path").join(__dirname, "cleaned", `${fnm}-cleaned.json`)
-    
-    
     fs.writeFileSync(newfile, JSON.stringify(obj, null, 4));
-
+    
     // console.log(obj);
 }
+let allfile = require("path").join(__dirname, "cleaned", `allfile.json`)
+fs.writeFileSync(allfile, JSON.stringify(allOBJ, null, 4));
